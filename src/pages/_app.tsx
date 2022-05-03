a170be21ef99d746e8d7f084c1598d39641abc7a
VarunVerma7 <varunverma855@gmail.com>
2022-05-03 02:48:35 -0400

    Charka and themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,27 @@ import "../styles/globals.css";
 
 import { Provider } from "react-redux";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import store from "../app/store";
+import { extendTheme } from "@chakra-ui/react";
 
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
 export default function MyApp({ Component, pageProps }: any) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </Provider>
   );
 }
