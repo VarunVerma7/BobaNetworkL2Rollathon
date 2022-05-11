44bca3bafce31a5b888010fe313093be0762e4ea
VarunVerma7 <varunverma855@gmail.com>
2022-05-10 23:03:15 -0400

    Get up early

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,22 +1,54 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-import "hardhat/console.sol";
+contract BobaGetUpEarly is ERC20 {
+    enum TimeZone {
+        EST,
+        PST
+    }
+    mapping(address => TimeZone) userTimezones;
+    address[] usersPlaying;
 
-contract Greeter {
-    string private greeting;
+    constructor() ERC20("GetUpEarly", "GUP") {}
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    function enterTimeZone(uint8 timezone) public {
+        if (timezone == 0) {
+            userTimezones[msg.sender] = TimeZone.EST;
+        } else {
+            userTimezones[msg.sender] = TimeZone.PST;
+        }
+        usersPlaying.push(msg.sender);
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
+    function redeemBobaGetUpEarly() public userRegistered {
+        // Figure out how to detect whether
+        bool userDidWakeUpEarly = true;
+        if (userDidWakeUpEarly) {
+            _mint(msg.sender, 5);
+        } else {
+            // Fuck them
+            _burn(msg.sender, balanceOf(msg.sender));
+        }
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    function redeamGUPForBobaMainnet() public userRegistered {
+        // 15 GUP = .1 Boba Mainnet
+        if (balanceOf(msg.sender) >= 15) {
+            _burn(msg.sender, 15);
+            payable(msg.sender).transfer(address(this).balance);
+        }
+    }
+
+    modifier userRegistered() {
+        bool userExists = false;
+        for (uint256 i = 0; i < usersPlaying.length; i++) {
+            if (usersPlaying[i] == msg.sender) {
+                userExists = true;
+            }
+        }
+        require(userExists, "You haven't entered your timezone");
+        _;
     }
 }
