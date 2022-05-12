71ff69e0384f24a282b58ad86ed65e06d95858fc
asharma13524 <asharma13524@gmail.com>
2022-05-12 13:36:29 -0400

    Pool for 30 days, calculate winner at end

--- contracts/DrinkBoba.sol
+++ contracts/DrinkBoba.sol
@@ -6,33 +6,38 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DrinkBoba is ERC20, Ownable {
-    struct BobaStruct {
-        bool drinkBoba;
-    }
-    mapping(address => BobaStruct) public userBobaLog;
+    mapping(address => uint256) public userBobaLog;
     address [] public usersPlaying;
     // did you drink boba tea today?
     constructor () ERC20("BobaTea", "BOBATEA") {}
 
-    // reset to false on new day,
-    // TODO: rethink design: prob more elegant way to do this
-    // probably want to keep history of users
-
-    // called from frontend when user enters Boba Tea
-    // probaly setApproval here as well
-    function registerUser() external {
+    // if user not signed up, append to usersPlaying and increment boba tea count
+    function incrementBobaCount() public {
+        for (uint256 i = 0; i < usersPlaying.length; i++) {
+            if (usersPlaying[i] == msg.sender) {
+                userBobaLog[msg.sender] += 1;
+                return;
+            }
+        }
         usersPlaying.push(msg.sender);
-
+        userBobaLog[msg.sender] += 1;
     }
 
-    function rewardUser() public userRegistered {
-        require(userBobaLog[msg.sender].drinkBoba == true);
-        _mint(msg.sender, 3);
+    function getUserBobaCount() public view userRegistered returns (uint256)  {
+        return userBobaLog[msg.sender];
     }
 
-    // can only be called by owner of contract, issue more bobatea
-    function issueMoreBobaTea() public onlyOwner {
-        _mint(msg.sender, 1000*10*18);
+    // return user with highest Boba Count
+    function calculateWinner() public view returns (address) {
+        uint256 highest = 0;
+        address winner;
+        for (uint256 i = 0; i < usersPlaying.length; i++) {
+            if(userBobaLog[usersPlaying[i]] > highest) {
+                highest = userBobaLog[usersPlaying[i]];
+                winner = usersPlaying[i];
+            }
+        }
+        return winner;
     }
 
     modifier userRegistered {
@@ -42,7 +47,12 @@ contract DrinkBoba is ERC20, Ownable {
                 userExists = true;
             }
         }
-        require(userExists, "You haven't entered your timezone");
+        require(userExists, "You haven't entered your Boba Tea consumption before!");
         _;
+    }
+
+    // can only be called by owner of contract, issue more bobatea
+    function issueMoreBobaTea() public onlyOwner {
+        _mint(msg.sender, 1000*10*18);
     }
 }
