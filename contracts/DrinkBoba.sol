a06196c349db8f5bab9c7a7614fb758c20056028
asharma13524 <asharma13524@gmail.com>
2022-05-17 13:41:43 -0400

    Adjust contract org given time constraints and additional complexity

--- contracts/DrinkBoba.sol
+++ contracts/DrinkBoba.sol
@@ -6,33 +6,39 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DrinkBoba is ERC20, Ownable {
+    // just keep this user speci
     mapping(address => uint256) public userBobaLog;
+    // keep track of if user has locked for 30 days
+    mapping(address => uint256) public lockTime;
     address [] public usersPlaying;
     uint256 public constant BOBA_PAYOUT = 10e16;
     uint256 public constant BTEA_REDEMPTION_AMOUNT = 5 * 10e18;
     uint256 public constant CONTRACT_APPROVAL_AMOUNT = 10000 * 10e18;
+    uint256 private duration = 30 days;
     constructor () ERC20("BobaTea", "BOBATEA") {}
 
-    // require user to deposit ~ $.02 in pool to register
     function registerUser() public payable {
-        require(msg.value == .000001 ether, "You must pay .000001 ether to register!");
+        // removing as will not make this a competition, at least for now
+        // require(msg.value == .000001 ether, "You must pay .000001 ether to register!");
         approve(address(this), CONTRACT_APPROVAL_AMOUNT);
         usersPlaying.push(msg.sender);
     }
 
     function incrementBobaCount() public userRegistered {
+        // TODO: add validation that user can only call once a day (maybe on frontend?)
         userBobaLog[msg.sender] += 1;
+        _mint(msg.sender, BTEA_REDEMPTION_AMOUNT);
     }
 
     function getUserBobaCount() public view userRegistered returns (uint256)  {
         return userBobaLog[msg.sender];
     }
 
-    // TODO: Add logic for when we want to call this (once a week/month?)
-    function declareWinner() public onlyOwner {
-        address winner = calculateWinner();
-        _mint(winner, BTEA_REDEMPTION_AMOUNT);
-    }
+    // // TODO: Add logic for when wewant to call this (once a week/month?)
+    // function declareWinner() public onlyOwner {
+    //     address winner = calculateWinner();
+    //     _mint(winner, BTEA_REDEMPTION_AMOUNT);
+    // }
 
     // TODO: rethink pattern here, should there be a param or frontend validates msg.value
     function redeemBobaTeaForBobaMainnet() public payable userRegistered {
@@ -43,20 +49,25 @@ contract DrinkBoba is ERC20, Ownable {
         }
     }
 
+    // calculate winner logic is harder, have to ensure every user enters pool
+    // before certain date, pool starts at same time, ends at same time, etc
+    // since this is proof of concept let's get rid of all of this and just
+    // allow user to redeem boba whenever. but need to have restrictions on how
+    // often they can increment boba amount
     // return user with highest Boba Count
-    function calculateWinner() public view returns (address) {
-        // TODO: handle edge cases (multiple winners?)
-        uint256 highest = 0;
-        address winner;
-        for (uint256 i = 0; i < usersPlaying.length; i++) {
-            if(userBobaLog[usersPlaying[i]] > highest) {
-                highest = userBobaLog[usersPlaying[i]];
-                winner = usersPlaying[i];
-            }
-        }
-        require(highest != 0, "No one drank boba tea! No one wins :(");
-        return winner;
-    }
+    // function calculateWinner() public view returns (address) {
+    //     // TODO: handle edge cases (multiple winners?)
+    //     uint256 highest = 0;
+    //     address winner;
+    //     for (uint256 i = 0; i < usersPlaying.length; i++) {
+    //         if(userBobaLog[usersPlaying[i]] > highest) {
+    //             highest = userBobaLog[usersPlaying[i]];
+    //             winner = usersPlaying[i];
+    //         }
+    //     }
+    //     require(highest != 0, "No one drank boba tea! No one wins :(");
+    //     return winner;
+    // }
 
     modifier userRegistered {
         bool userExists = false;
